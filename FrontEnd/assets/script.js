2d790a7e3d1269c314714285ae3ae3ea14f530e4
sulirb <121161440+sulirb@users.noreply.github.com>
2023-01-31 15:05:57 +0100

    filtres terminés

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,8 +1,7 @@
-async function genererWorks() {
-  const response = await fetch("http://localhost:5678/api/works");
-  const data = await response.json();
-  console.log(data);
+const response = await fetch("http://localhost:5678/api/works");
+const data = await response.json();
 
+function genererWorks(data) {
   const sectionGallery = document.querySelector(".gallery");
 
   for (let i = 0; i < data.length; i++) {
@@ -12,6 +11,8 @@ async function genererWorks() {
     // Création des balises
     const imageElement = document.createElement("img");
     imageElement.src = article.imageUrl;
+    // Affichage des images
+    imageElement.setAttribute("crossorigin", "anonymous");
     const titleElement = document.createElement("p");
     titleElement.innerText = article.title;
 
@@ -22,4 +23,48 @@ async function genererWorks() {
   }
 }
 
-genererWorks();
+genererWorks(data);
+
+// Reinitialisation
+const boutonTous = document.querySelector(".btn-tous");
+
+boutonTous.addEventListener("click", function () {
+  const worksFiltered = data.filter(function (work) {
+    return work.categoryId === 1, 2, 3;
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  genererWorks(worksFiltered);
+});
+
+// Installation filtre 1
+const boutonObjet = document.querySelector(".btn-objet");
+
+boutonObjet.addEventListener("click", function () {
+  const worksFiltered = data.filter(function (work) {
+    return work.categoryId === 1;
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  genererWorks(worksFiltered);
+});
+
+// Installation filtre 2
+const boutonAppart = document.querySelector(".btn-appart");
+
+boutonAppart.addEventListener("click", function () {
+  const worksFiltered = data.filter(function (work) {
+    return work.categoryId === 2;
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  genererWorks(worksFiltered);
+});
+
+// Installation filtre 3
+const boutonHotel = document.querySelector(".btn-hotel");
+
+boutonHotel.addEventListener("click", function () {
+  const worksFiltered = data.filter(function (work) {
+    return work.categoryId === 3;
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  genererWorks(worksFiltered);
+});
