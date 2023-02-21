d2043b07690a54db59cb5420a6b4cca4bd3def58
sulirb <121161440+sulirb@users.noreply.github.com>
2023-02-21 18:05:22 +0100

    suppression travaux (mais avec refresh)

--- FrontEnd/assets/modale.js
+++ FrontEnd/assets/modale.js
@@ -1,10 +1,11 @@
 import { fetchWorks, deleteWorks } from "./api.js";
 
-// Création de la logique d'ouverture et fermeture de la modale
+// Création de la logique d'ouverture de la modale
 
 const myDialog = document.getElementById("myDialog");
 const myButton = document.getElementById("myButton");
 const closeButton = document.getElementById("js-modal-close");
+// const deleteAll = document.getElementById("delete-all");
 const body = document.body;
 
 // Ouvre la fenêtre modale lorsqu'on clique sur le bouton
@@ -12,6 +13,71 @@ function openModal() {
   return myDialog.showModal();
 }
 myButton.addEventListener("click", openModal);
+
+async function main() {
+  const data = await fetchWorks();
+  createWorks(data);
+}
+
+main();
+
+// Création des éléments dans la modale
+
+const modaleGallery = document.querySelector(".modal-gallery");
+
+function createWorks(data) {
+  for (const article of data) {
+    // Création des works
+    const worksElement = modaleGallery.appendChild(
+      document.createElement("article")
+    );
+
+    const imageElement = worksElement.appendChild(
+      document.createElement("img")
+    );
+    imageElement.src = article.imageUrl;
+    imageElement.setAttribute("crossorigin", "anonymous");
+
+    const titleElement = worksElement.appendChild(document.createElement("p"));
+    titleElement.innerText = "éditer";
+
+    // Extrait l'id de chaque work
+    worksElement.setAttribute("data-id", article.id);
+
+    addDeleteButton(worksElement);
+    addDeleteAll(worksElement);
+  }
+}
+
+function addDeleteButton(worksElement) {
+  // Création du bouton de suppression sur les éléments
+  const deleteButton = worksElement.appendChild(
+    document.createElement("button")
+  );
+  deleteButton.innerHTML = '<i class="fa fa-solid fa-trash-can"></i>';
+
+  // Supression des éléments sur la modale
+  deleteButton.addEventListener("click", async function (e) {
+    // Utilise l'id d'un work pour le supprimer
+    const id = worksElement.getAttribute("data-id");
+    deleteWorks(id);
+    worksElement.remove();
+    e.preventDefault();
+  });
+}
+
+function addDeleteAll() {
+  const deleteAll = document.getElementById("delete-all");
+  deleteAll.addEventListener("click", async function (e) {
+    e.preventDefault();
+    const worksElements = document.querySelectorAll("[data-id]");
+    for (let i = 0; i < worksElements.length; i++) {
+      const id = worksElements[i].getAttribute("data-id");
+      await deleteWorks(id);
+      worksElements[i].remove(); // supprime l'élément du DOM
+    }
+  });
+}
 
 // Ferme la fenêtre modale lorsqu'on clique sur le bouton de fermeture
 function closeModal() {
@@ -25,47 +91,3 @@ body.addEventListener("click", (event) => {
     closeModal();
   }
 });
-
-// Création des éléments dans la modale
-
-async function main() {
-  const data = await fetchWorks();
-  createWorks(data);
-}
-
-main();
-
-const sectionGallery = document.querySelector(".modal-gallery");
-
-function createWorks(data) {
-  for (const article of data) {
-    // Création des works
-    const worksElement = sectionGallery.appendChild(
-      document.createElement("article")
-    );
-
-    const imageElement = worksElement.appendChild(
-      document.createElement("img")
-    );
-    imageElement.src = article.imageUrl;
-    imageElement.setAttribute("crossorigin", "anonymous");
-
-    const titleElement = worksElement.appendChild(document.createElement("p"));
-    titleElement.innerText = "éditer";
-
-    addDeleteButton(worksElement);
-  }
-
-  function addDeleteButton(worksElement) {
-    // Création du bouton de suppression sur les éléments
-    const deleteButton = worksElement.appendChild(
-      document.createElement("button")
-    );
-    deleteButton.innerHTML = '<i class="fa fa-solid fa-trash-can"></i>';
-    // Supression des éléments sur la modale
-    deleteButton.addEventListener("click", async function (e) {
-      e.preventDefault();
-      deleteWorks(`${id}`);
-    });
-  }
-}
