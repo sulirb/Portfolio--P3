16211ddad5d281981895f69006a13e3a65e75152
sulirb <121161440+sulirb@users.noreply.github.com>
2023-02-22 17:14:43 +0100

    création de la 2eme modale

--- FrontEnd/assets/modale.js
+++ FrontEnd/assets/modale.js
@@ -5,7 +5,6 @@ import { fetchWorks, deleteWorks } from "./api.js";
 const myDialog = document.getElementById("myDialog");
 const myButton = document.getElementById("myButton");
 const closeButton = document.getElementById("js-modal-close");
-// const deleteAll = document.getElementById("delete-all");
 const body = document.body;
 
 // Ouvre la fenêtre modale lorsqu'on clique sur le bouton
@@ -49,6 +48,7 @@ function createWorks(data) {
   }
 }
 
+// Supprime un ID
 function addDeleteButton(worksElement) {
   // Création du bouton de suppression sur les éléments
   const deleteButton = worksElement.appendChild(
@@ -61,33 +61,79 @@ function addDeleteButton(worksElement) {
     // Utilise l'id d'un work pour le supprimer
     const id = worksElement.getAttribute("data-id");
     deleteWorks(id);
-    worksElement.remove();
+    worksElement.remove(); // supprime l'élément du DOM (supprime avant de refresh)
     e.preventDefault();
   });
 }
 
+// Supprime tous les ID
 function addDeleteAll() {
   const deleteAll = document.getElementById("delete-all");
   deleteAll.addEventListener("click", async function (e) {
-    e.preventDefault();
     const worksElements = document.querySelectorAll("[data-id]");
+    // Boucle sur chaque data id pour pouvoir les supprimer d'un coup
     for (let i = 0; i < worksElements.length; i++) {
       const id = worksElements[i].getAttribute("data-id");
       await deleteWorks(id);
-      worksElements[i].remove(); // supprime l'élément du DOM
+      worksElements[i].remove(); // supprime les éléments du DOM (supprime avant de refresh)
+      e.preventDefault();
     }
   });
 }
 
-// Ferme la fenêtre modale lorsqu'on clique sur le bouton de fermeture
-function closeModal() {
-  return myDialog.close();
+// Création de la 2ème page modale
+const addPhotoButton = document.getElementById("add-photo");
+const myDialogTwo = document.getElementById("myDialogTwo");
+const closeButtonTwo = document.getElementById("js-modal-close-two");
+
+function changeModal() {
+  myDialog.close();
+  myDialogTwo.showModal();
 }
-closeButton.addEventListener("click", closeModal);
+
+addPhotoButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  changeModal();
+});
+
+// Fonction générale pour fermer la modale
+function closeModal(dialog) {
+  return dialog.close();
+}
+
+// Ferme la fenêtre modale lorsqu'on clique sur le bouton de fermeture
+closeButton.addEventListener("click", function () {
+  closeModal(myDialog);
+});
 
 // Ferme la fenêtre modale lorsqu'on clique en dehors de la fenêtre
-body.addEventListener("click", (event) => {
+body.addEventListener("click", function (event) {
   if (event.target === myDialog) {
-    closeModal();
+    closeModal(myDialog);
   }
+});
+
+// Ajoute les mêmes fonctionnalités pour la 2ème modale
+closeButtonTwo.addEventListener("click", function () {
+  closeModal(myDialogTwo);
+});
+
+body.addEventListener("click", function (event) {
+  if (event.target === myDialogTwo) {
+    closeModal(myDialogTwo);
+  }
+});
+
+// Revient à la 1ère modale
+
+const backMyDialog = document.getElementById("back-to-one");
+
+function changeModalTwo() {
+  myDialogTwo.close();
+  myDialog.showModal();
+}
+
+backMyDialog.addEventListener("click", function (e) {
+  e.preventDefault();
+  changeModalTwo();
 });
